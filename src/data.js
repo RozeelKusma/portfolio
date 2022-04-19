0f8ce172d1a58547d91a99aade4e9f94c2af286e
RozeelKusma <kusmarozeel@gmail.com>
2022-04-19 19:31:26 +0545

    changes

--- src/data.js
+++ src/data.js
@@ -1,24 +1,29 @@
+import img1 from './1.png';
+import img2 from './2.png';
+import img3 from './3.jpg';
+import img4 from './4.png';
+
 
 
 export const products = [
     {
       id: 1,
-      img: require("/home/rozeel/project/portfolio/src/1.png"),
+      img: require("./1.png"),
       link: "https://github.com/RozeelKusma/taxpay",
     },
     {
       id: 2,
-      img: require("/home/rozeel/project/portfolio/src/2.png"),
+      img: require("./2.png"),
       link: "https://github.com/RozeelKusma/reacttest",
     },
     {
       id: 3,
-      img: require("/home/rozeel/project/portfolio/src/3.jpg"),
+      img: require("./3.jpg"),
       link: "https://github.com/RozeelKusma/lisence",
     },
     {
       id: 4,
-      img: require("/home/rozeel/project/portfolio/src/4.png"),
-      link: "https://github.com/RozeelKusma/versify",
+      img: require("./4.png"),
+      link: "https://github.com/RozeelKusma/versify-frontend",
     }
   ];
