a79c1700053564f175b5dfdefb5a6afa6208c5c5
RozeelKusma <kusmarozeel@gmail.com>
2022-04-19 18:30:22 +0545

    added versify

--- src/data.js
+++ src/data.js
@@ -16,4 +16,9 @@ export const products = [
       img: require("/home/rozeel/project/portfolio/src/3.jpg"),
       link: "https://github.com/RozeelKusma/lisence",
     },
+    {
+      id: 4,
+      img: require("/home/rozeel/project/portfolio/src/4.jpg"),
+      link: "https://github.com/RozeelKusma/versify",
+    }
   ];
